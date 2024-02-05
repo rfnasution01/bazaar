fe5f85a5ff206b815d7f472757b92540bb79b6fb
rfnasution01 <rivaifnasution@gmail.com>
2024-02-05 17:03:22 +0700

    fix: const interval

--- src/const/filter-interval.tsx
+++ src/const/filter-interval.tsx
@@ -1,11 +1,1 @@
-export const listInterval = [
-  "m1",
-  "m5",
-  "m15",
-  "m30",
-  "h1",
-  "h2",
-  "h6",
-  "h12",
-  "d1",
-];
+export const listInterval = ["m1", "m5", "m30", "h1", "h6", "h12", "d1"];
